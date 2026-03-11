443a3447711e6f5df3fa4b87c9e62b0bd38b8ab6
Bubba J Bohnson <octagonmb@gmail.com>
2026-03-11 13:00:44 -0700

    added audio manager class

---
+++ FreeWill/scripts/autoload/audio_manager.gd
@@ -0,0 +1,38 @@
+extends Node
+
+## AudioManager class.
+
+
+func play_sound_at(position: Vector3, sound: AudioStream, volume_db: float = 0.0, randomize_pitch: bool = true) -> void:
+	var new_sound_player := AudioStreamPlayer3D.new()
+
+	new_sound_player.bus = "SFX"
+	new_sound_player.attenuation_filter_cutoff_hz = 20000
+
+	if (randomize_pitch):
+		new_sound_player.pitch_scale = randf_range(0.92, 1.13)
+
+	new_sound_player.stream = sound
+	new_sound_player.volume_db = volume_db
+	new_sound_player.finished.connect(new_sound_player.queue_free)
+
+	add_child(new_sound_player)
+	new_sound_player.position = position
+	new_sound_player.play()
+
+
+func play_sound(sound: AudioStream, volume_db: float = 0.0, randomize_pitch: bool = true) -> void:
+	var new_sound_player := AudioStreamPlayer.new()
+
+	new_sound_player.bus = "SFX"
+	new_sound_player.attenuation_filter_cutoff_hz = 20000
+
+	if (randomize_pitch):
+		new_sound_player.pitch_scale = randf_range(0.92, 1.13)
+
+	new_sound_player.stream = sound
+	new_sound_player.volume_db = volume_db
+	new_sound_player.finished.connect(new_sound_player.queue_free)
+
+	add_child(new_sound_player)
+	new_sound_player.play()
