c3548ee73ce1998cd54496f4c45f2e7aa1c6752e
TitaniumLili <liliviawong@protonmail.com>
2026-03-17 21:22:37 +1300

    fixed compat particle colours

--- FreeWill/scripts/effects/cannon_particles.gd
+++ FreeWill/scripts/effects/cannon_particles.gd
@@ -11,3 +11,9 @@ func _ready() -> void:
 	cannon_particles.restart()
 
 	smoke_particles.finished.connect(queue_free)
+	var renderer : String = RenderingServer.get_current_rendering_method()
+	var mat : StandardMaterial3D = cannon_particles.draw_pass_1.surface_get_material(0)
+	if renderer == "forward_plus":
+		mat.albedo_color = Color(1.0, 0.337, 0.0)
+	else:
+		mat.albedo_color = Color(1.0, 0.005, 0.0)
