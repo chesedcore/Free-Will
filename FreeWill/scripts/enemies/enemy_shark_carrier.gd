eb3074bba9d66fb0265b8ba117a507f6058c1036
TitaniumLili <liliviawong@protonmail.com>
2026-03-28 23:42:15 +1300

    updated models

--- FreeWill/scripts/enemies/enemy_shark_carrier.gd
+++ FreeWill/scripts/enemies/enemy_shark_carrier.gd
@@ -1,11 +1,16 @@
 class_name EnemySharkCarrier extends BaseEnemy
 const SHARK = preload("res://scenes/entities/combat/shark.tscn")
+const ENEMY_SHIP_BULLET = preload("res://scenes/entities/combat/enemy_ship_bullet.tscn")
 
 var player : PlayerTank
 var rotation_speed: float = 5.0
 @export var base: MeshInstance3D
 @export var shark_spawn_location: Node3D
 @export var attack_timer: Timer
+
+@export var canon_aims : Array[RayCast3D]
+@export var cannon_bases : Array[Node3D]
+@export var cannon_barrels : Array[Node3D]
 
 func _physics_process(delta: float) -> void:
 	if player:
@@ -30,6 +35,21 @@ func _on_line_of_sight_body_exited(body: Node3D) -> void:
 			player = null
 			attack_timer.stop()
 
+func shoot(cannon_index: float)->void:
+	var particles: Node3D = \
+		preload("res://scenes/entities/tank_cannon_particles.tscn").instantiate()
+	particles.rotation_degrees.y -= 90
+	canon_aims[cannon_index].add_child.call_deferred(particles)
+	
+	var new_bullet: Enemy_bullet = ENEMY_SHIP_BULLET.instantiate()
+	new_bullet.position =  canon_aims[cannon_index].global_position
+
+	new_bullet.target = player
+
+	# Monarch: Usually I'd make a dedicated `Bullets` Node3D that 'holds' this node as an array,
+	# then trigger a signal that makes the World handler add the bullet to the Bullets node.
+	# But for now (in the spirit of this jam), this will do just fine.
+	EventBus.spawn_weaponry.emit(new_bullet)
 
 func _on_attack_timer_timeout() -> void:
 	if player:
