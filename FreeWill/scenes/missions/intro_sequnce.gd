6d881aac46582ef0f0b2d872c113fb150219afa8
Gael <farhanalnoor43@gmail.com>
2026-03-29 00:18:27 +0500

    skip intro

--- FreeWill/scenes/missions/intro_sequnce.gd
+++ FreeWill/scenes/missions/intro_sequnce.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 const MISSION_1_ACT_1 = preload("res://scenes/missions/mission_1_act1.tscn")
+@export var animation_player: AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +13,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		
+		animation_player.advance(10)
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	get_tree().change_scene_to_packed(MISSION_1_ACT_1)
