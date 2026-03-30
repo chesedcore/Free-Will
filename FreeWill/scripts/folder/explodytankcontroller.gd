5333ab16e6463f8cf0c28c1d88c2031e1982a37e
TitaniumLili <liliviawong@protonmail.com>
2026-03-30 14:19:43 +1300

    logo and thumbnail

--- FreeWill/scripts/folder/explodytankcontroller.gd
+++ FreeWill/scripts/folder/explodytankcontroller.gd
@@ -7,6 +7,9 @@ var flying : bool = false
 var falling : bool = false
 const START_POS = Vector2(200, 400)
 
+@export var particles : GPUParticles2D
+@export var particles2 : GPUParticles2D
+
 
 func _ready() -> void:
 	reset()
@@ -14,8 +17,8 @@ func _ready() -> void:
 func reset() -> void:
 	falling = false
 	flying = false
-	position = START_POS
-	set_rotation(0)
+	global_position = START_POS
+	set_rotation(0.)
 	
 func _physics_process(delta: float) -> void:
 	if flying or falling:
@@ -25,12 +28,10 @@ func _physics_process(delta: float) -> void:
 		if flying:
 			set_rotation(deg_to_rad(velocity.y * -0.05) + 0.4)
 		elif falling:
-			set_rotation(0)
+			set_rotation(0.)
 		move_and_collide(velocity * delta)
 
 func explode() -> void:
 	velocity.y = EXPLODE_SPEED
-	$GPUParticles2D.restart()
-	$GPUParticles2D2.restart()
-	
-		
+	particles.restart()
+	particles2.restart()
