e85854c33a13d91c1991aa8fc9c841bec577921f
TitaniumLili <liliviawong@protonmail.com>
2026-03-31 21:20:38 +1300

    add more shit to loading shit

--- FreeWill/scripts/ui/loading_shit.gd
+++ FreeWill/scripts/ui/loading_shit.gd
@@ -2,8 +2,13 @@ class_name LoadingShit extends Node3D
 
 signal finished
 
+@export var charge_particles : GPUParticles3D
+@export var charge_spark_particles : GPUParticles3D
+
 func _ready() -> void:
 	var tree := get_tree()
+	charge_particles.restart()
+	charge_spark_particles.restart()
 	RailgunParticles.compile_particles(self)
 	CannonParticles.compile_particles(self)
 	ExplosionParticles.compile_particles(self)
